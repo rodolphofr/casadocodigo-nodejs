8b0c8c100dea16c9b425836927edfc4f2d082387
rodolphofr <rodolpho.frodrigues@gmail.com>
2018-03-02 13:30:11 -0300

    * Impl middlewares to render errors page

--- config/express.js
+++ config/express.js
@@ -10,10 +10,12 @@ module.exports = () => {
     app.set('view engine', 'ejs'); // informa para o app a view engine utilizada
     app.set('views', './app/views'); // informa para o app aonde estao as views
 
+    // Os Middlewares são funções cujo objetivo é possibilitar a interceptação do 
+    // request e adicionar verificações e comportamentos sobre eles.
+
     // Garante que todo arquivo que estiver dentro da pasta public será servido de 
-    // maneira estática sem que precise de uma rota configurada para ele.
+    // maneira estática sem que precise de uma rota configurada par a ele.
     app.use(express.static('./app/public'));
-    /* MIDDLEWARES */
     // request middleware
     // informando que o express agora é o responsável por fazer parse de urlencoded
     app.use(bodyParser.urlencoded({ extended : true }))
@@ -33,6 +35,16 @@ module.exports = () => {
         .then('infra')
         .then('validators')
         .into(app);
+
+    app.use((request, response, next) => {
+        response.status(400).render('errors/400');
+        next();
+    });
+
+    app.use((error, request, response, next) => {
+        response.status(500).render('errors/500');
+        next(error);
+    });
 
     return app;
 }
